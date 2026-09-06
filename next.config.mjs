a820232068c31402e6b8039c1270854bde0cc50f
ayzentsolutions <ayzentsolutions@gmail.com>
2026-09-07 02:35:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,54 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: [
+      "image/avif",
+      "image/webp",
+    ],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "res.cloudinary.com",
+      },
+
+      {
+        protocol: "https",
+        hostname:
+          "images.unsplash.com",
+      },
+    ],
+
+    deviceSizes: [
+      320,
+      420,
+      640,
+      768,
+      1024,
+      1280,
+      1536,
+      1920,
+    ],
+
+    imageSizes: [
+      16,
+      32,
+      48,
+      64,
+      96,
+      128,
+      256,
+      384,
+    ],
   },
+
+  compress: true,
+
+  poweredByHeader: false,
 };
 
 export default nextConfig;
